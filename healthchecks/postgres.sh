3aa7f825c8b9c2ac1032bae3905338655f6438aa
Rajaram Iyyappan <105640321+Devops-Raja@users.noreply.github.com>
2026-06-15 14:13:25 +0530

    local fix

--- healthchecks/postgres.sh
+++ healthchecks/postgres.sh
@@ -6,7 +6,7 @@ user="${POSTGRES_USER:-postgres}"
 db="${POSTGRES_DB:-postgres}"
 
 # Use pg_isready to verify network availability without full query execution overhead
-if pg_isready -h 127.0.0.1 -U "$user" -d "$db"; then
+if pg_isready -U 127.0.0.1 -U "$user" -d "$db"; then
     exit 0
 fi
 
